192076b95b8ab80b4b696702d52d3e8696473f83
Rashit Gareyev <rashit.gareev@akvelon.com>
2022-02-17 16:07:04 +0300

    basic auth to swagger ui added

---
+++ src/main/java/com/pet/project/awss3client/config/ResourceServerConfig.java
@@ -0,0 +1,18 @@
+package com.pet.project.awss3client.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/aws-s3/swagger-ui").authenticated();
+    }
+}
---
+++ src/main/java/com/pet/project/awss3client/config/SpringFoxConfig.java
@@ -0,0 +1,57 @@
+package com.pet.project.awss3client.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SpringFoxConfig {
+
+    @Value("${aws-s3-kafka.base-package}")
+    private String basePackage;
+
+    public static final String BASIC_AUTH = "basicAuth";
+
+    @Bean
+    public Docket api() {
+
+        SecurityReference securityReference = SecurityReference.builder()
+                .reference(BASIC_AUTH)
+                .scopes(new AuthorizationScope[0])
+                .build();
+
+        ArrayList<SecurityReference> reference = new ArrayList<>(1);
+        reference.add(securityReference);
+
+        ArrayList<SecurityContext> securityContexts = new ArrayList<>(1);
+        securityContexts.add(SecurityContext.builder().securityReferences(reference).build());
+
+        ArrayList<SecurityScheme> auth = new ArrayList<>(1);
+        auth.add(new BasicAuth(BASIC_AUTH));
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(auth)
+                .securityContexts(securityContexts)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(basePackage))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfoBuilder builder = new ApiInfoBuilder();
+        return builder.title("Rent car service")
+                .version("1.0")
+                .build();
+    }
+}
